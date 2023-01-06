4e121e13623a18bd5022cc8d232040a0c617d0e1
Ivan Plakhutin <i.plakhutin@gmail.com>
2023-01-06 14:15:43 +0200

    Create navbar

--- ui/src/app/modules/app-routing.module.ts
+++ ui/src/app/modules/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent},
+  { path: '**', redirectTo: 'navbar', pathMatch: 'full' }
+];
 
 
 
