406be9c97b22585ee86d9496be413797e156b269
Ivan Plakhutin <i.plakhutin@gmail.com>
2023-01-08 22:06:39 +0200

    target text

--- ui/src/app/modules/app-routing.module.ts
+++ ui/src/app/modules/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { MainComponent } from '../components/main/main.component';
 import { TargetComponent } from '../components/target/target.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { FavourComponent } from '../components/favour/favour.component';
+import { AuditComponent } from '../components/audit/audit.component';
 
 const routes: Routes = [
   { path: 'navbar', component: NavbarComponent},
   { path: 'main', component: MainComponent},
   { path: 'target', component: TargetComponent},
+  { path: 'favour', component: FavourComponent},
+  { path: 'audit', component: AuditComponent},
+  { path: 'footer', component: FooterComponent},
   { path: '**', redirectTo: 'main', pathMatch: 'full' }
 ];
 
