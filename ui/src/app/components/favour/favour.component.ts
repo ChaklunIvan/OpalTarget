7c3238851a7ea087f491e411a795eaff7d0de432
Ivan Plakhutin <i.plakhutin@gmail.com>
2023-01-09 12:44:33 +0200

    create modal form

--- ui/src/app/components/favour/favour.component.ts
+++ ui/src/app/components/favour/favour.component.ts
@@ -6,13 +6,17 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './favour.component.html',
   styleUrls: ['./favour.component.css']
 })
+
 export class FavourComponent {
+
+  modalTitle = "";
 
   constructor(private modalService: NgbModal) { }
 
-  openVerticallyCentered(content: any) {
+  openVerticallyCentered(content: any, event: any) {
+    let target = event.target || event.srcElement || event.currentTarget;
+    this.modalTitle = target.getAttribute('value');
     this.modalService.open(content, { centered: true });
   }
-
 }
 
