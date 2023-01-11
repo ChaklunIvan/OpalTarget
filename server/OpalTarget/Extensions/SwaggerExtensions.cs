13aa9db6a8e79b1dc0f34681b5f74dcd4f1340a0
Ivan Plakhutin <i.plakhutin@gmail.com>
2023-01-11 13:08:55 +0200

    swagger config

--- server/OpalTarget/Extensions/SwaggerExtensions.cs
+++ server/OpalTarget/Extensions/SwaggerExtensions.cs
@@ -1,6 +1,16 @@
-﻿namespace OpalTarget.Extensions
+﻿using Microsoft.OpenApi.Models;
+
+namespace OpalTarget.Extensions
 {
-    public class SwaggerExtensions
+    public static class SwaggerExtensions
     {
+        public static IServiceCollection AddSwaggerGenerator(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OpalTarget API", Version = "v1" });
+            });
+            return services;
+        }
     }
 }
